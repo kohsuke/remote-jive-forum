172756aa281d5d823c81923ff0071f753106e1cf
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-03-25 23:42:49 +0000

    renamed the test code.

---
+++ src/test/Main.java
@@ -0,0 +1,19 @@
+
+import com.meterware.httpunit.WebConversation;
+import org.kohsuke.remotejiveforums.Forum;
+import org.kohsuke.remotejiveforums.Topic;
+
+import java.net.URL;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        WebConversation wc = new WebConversation();
+        wc.setProxyServer("webcache.sfbay.sun.com",8080);
+        Forum f = Forum.get(new URL("http://www.jivesoftware.com/jive/"),5,wc);
+        Topic topic = f.createTopic("test","testing. please ignore");
+        System.out.println(topic.getTopicURL());
+    }
+}
